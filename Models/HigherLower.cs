8d046333dd7bfc76568243aff879196ad359cfe3
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-24 21:31:38 -0700

    Fix _max number since maximum number of Random class is exclusive.

--- Models/HigherLower.cs
+++ Models/HigherLower.cs
@@ -6,7 +6,7 @@ namespace HighLowGame.Models
   {
     private static int _number;
     private static int _min = 1;
-    private static int _max = 100;
+    private static int _max = 101;
 
     public static int GetNumber()
     {
@@ -23,13 +23,14 @@ namespace HighLowGame.Models
       return _max;
     }
 
+    // Random class ref: https://docs.microsoft.com/en-us/dotnet/api/system.random?redirectedfrom=MSDN&view=netframework-4.8
     public static void initialGuess()
     {
       Random randomNum = new Random();
       _number = randomNum.Next(_min, _max);
     }
 
-    public static void highGuess(int lowNum)
+    public static void guessForHigher(int lowNum)
     {
       Random randomNum = new Random();
       if (lowNum > _min)
@@ -39,7 +40,7 @@ namespace HighLowGame.Models
       _number = randomNum.Next(_min, _max);
     }
 
-    public static void lowGuess(int highNum)
+    public static void guessForLower(int highNum)
     {
       Random randomNum = new Random();
       if(highNum < _max)
